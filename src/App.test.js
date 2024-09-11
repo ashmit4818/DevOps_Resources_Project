14d189e1fd2d5f5d68fa89937366515aa9e970d1
Ashmit Dwivedi <manudwivedi777@gmail.com>
2024-09-12 01:05:50 +0530

    test library v modified

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { act } from 'react';  // New import
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  
+test('renders component correctly', () => {
+  act(() => {
+    render(<App />);
+  });
 });
